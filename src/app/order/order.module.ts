afb54c42fc54769b64df16bc30b03cf4fb769da8
ilyass <ajdainii@gmail.com>
2021-10-14 11:50:53 +0100

    order component

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
 import { ListOrderComponent } from './components/list-order/list-order.component';
+import { AddOrderComponent } from './components/add-order/add-order.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [ListOrderComponent],
+  declarations: [ListOrderComponent, AddOrderComponent],
   imports: [
     CommonModule,
-    OrderRoutingModule
+    OrderRoutingModule,
+    SharedModule
   ]
 })
 export class OrderModule { }
